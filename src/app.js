74d2a8ab93a0ba9a33e37ff9084e78e6682293c9
alexhyriavets <alexhyriavets@gmail.com>
2018-06-24 22:49:25 +0300

    add user model

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(require('morgan')('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+mongoose.connect('mongodb://localhost/backend-boilerplate')
+mongoose.set('debug', true)
+
+require('./models/User');
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500)
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    })
+  })
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500)
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  })
+})
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'))
